194ef97384a3ec517a6e4ae5539e5441d23ecd80
Antonyuk Vadim <vyantonyuk@edu.hse.ru>
2025-09-18 14:48:52 +0300

    Done varint task

--- tasks/varint/varint.cpp
+++ tasks/varint/varint.cpp
@@ -1,6 +1,25 @@
 #include "varint.h"
 
 size_t DecodeVarint(const uint8_t* data, size_t size, uint64_t& result) {
-    result = 0;
-    return 0;
+    uint64_t tmp = 0;
+    size_t current_block = 0;
+    while (current_block < size) {
+        for (size_t bit_offset = 0; bit_offset < 7; ++bit_offset) {
+            if ((data[current_block] & (1 << bit_offset)) == 0) {
+                continue;
+            }
+            if (bit_offset + current_block * 7 > 63) {
+                return 0;
+            }
+            tmp |= (1ull << (bit_offset + current_block * 7));
+        }
+        if ((data[current_block++] & (1 << 7)) == 0) {
+            break;
+        }
+        if (current_block == size) {
+            return 0;
+        }
+    }
+    result = tmp;
+    return current_block;
 }
