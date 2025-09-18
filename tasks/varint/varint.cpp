661c97133ce06dd4e0017bafa5ff354a3b8575c0
Antonyuk Vadim <vyantonyuk@edu.hse.ru>
2025-09-18 15:10:20 +0300

    Fix magic numbers varint task

--- tasks/varint/varint.cpp
+++ tasks/varint/varint.cpp
@@ -1,19 +1,21 @@
 #include "varint.h"
 
 size_t DecodeVarint(const uint8_t* data, size_t size, uint64_t& result) {
+    const size_t BLOCK_SIZE = 7;
+    const size_t RESULT_SIZE = 64;
     uint64_t tmp = 0;
     size_t current_block = 0;
     while (current_block < size) {
-        for (size_t bit_offset = 0; bit_offset < 7; ++bit_offset) {
+        for (size_t bit_offset = 0; bit_offset < BLOCK_SIZE; ++bit_offset) {
             if ((data[current_block] & (1 << bit_offset)) == 0) {
                 continue;
             }
-            if (bit_offset + current_block * 7 > 63) {
+            if (bit_offset + current_block * BLOCK_SIZE >= RESULT_SIZE ) {
                 return 0;
             }
-            tmp |= (1ull << (bit_offset + current_block * 7));
+            tmp |= (1ull << (bit_offset + current_block * BLOCK_SIZE));
         }
-        if ((data[current_block++] & (1 << 7)) == 0) {
+        if ((data[current_block++] & (1 << BLOCK_SIZE)) == 0) {
             break;
         }
         if (current_block == size) {
