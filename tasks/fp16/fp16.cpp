e1c2131e974c1e24505e1c1ce3ca6244fa1353e8
Antonyuk Vadim <vyantonyuk@edu.hse.ru>
2025-09-19 22:45:28 +0300

    fp16 fix

--- tasks/fp16/fp16.cpp
+++ tasks/fp16/fp16.cpp
@@ -1,30 +1,30 @@
 #include "fp16.h"
 
+#include <algorithm>
 #include <limits>
 #include <cmath>
 
 float ConvertFloat16ToFloat(uint16_t float16_bits) {
-    const int32_t mantissa_size = 10;
-    const int32_t exponent_size = 5;
-    const int32_t exponent_bias = 15;
-    const int32_t mantissa = float16_bits & ((1 << mantissa_size) - 1);
-    float16_bits >>= mantissa_size;
-    const int32_t exponent = float16_bits & ((1 << exponent_size) - 1);
-    float16_bits >>= exponent_size;
-    float sign = (float16_bits ? -1.f : 1.f);
-    const uint32_t nan_exponent = 0b11111;
-    if (exponent == nan_exponent) {
+    static constexpr int32_t MantissaSize = 10;
+    static constexpr int32_t ExponentSize = 5;
+    static constexpr int32_t ExponentBias = 15;
+    static constexpr int32_t MantissaMask = (1 << MantissaSize) - 1;
+    static constexpr float MantissaMax = 1024.f;
+    static constexpr uint32_t NanExponent = 0b11111;
+
+    const int32_t mantissa = float16_bits & MantissaMask;
+    float16_bits >>= MantissaSize;
+    const int32_t exponent = float16_bits & ((1 << ExponentSize) - 1);
+    float16_bits >>= ExponentSize;
+    const float sign = (float16_bits ? -1.f : 1.f);
+
+    if (exponent == NanExponent) {
         if (mantissa) {
             return std::numeric_limits<float>::quiet_NaN();
         }
         return std::numeric_limits<float>::infinity() * sign;
     }
-    if (exponent == 0) {
-        if (!mantissa) {
-            return 0.f * sign;
-        }
-        return std::ldexp(static_cast<float>(mantissa), -(mantissa_size + exponent_bias - 1)) * sign;
-    }
-    return sign * static_cast<float>(
-                      std::ldexp(static_cast<float>(mantissa) / pow(2, mantissa_size) + 1.f, exponent - exponent_bias));
+
+    const float mantissa_32 = static_cast<float>(mantissa) / MantissaMax + static_cast<float>(exponent != 0);
+    return sign * std::ldexp(mantissa_32, std::max(exponent, 1) - ExponentBias);
 }
