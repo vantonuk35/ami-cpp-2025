1815d85e70029d5a9a967989c99f1f796fc53af0
Antonyuk Vadim <vyantonyuk@edu.hse.ru>
2025-09-20 11:52:35 +0300

    fp16 fix

--- tasks/fp16/fp16.cpp
+++ tasks/fp16/fp16.cpp
@@ -9,12 +9,13 @@ float ConvertFloat16ToFloat(uint16_t float16_bits) {
     static constexpr int32_t ExponentSize = 5;
     static constexpr int32_t ExponentBias = 15;
     static constexpr int32_t MantissaMask = (1 << MantissaSize) - 1;
+    static constexpr int32_t ExponentMask = (1 << MantissaSize) - 1;
     static constexpr float MantissaMax = 1024.f;
     static constexpr uint32_t NanExponent = 0b11111;
 
     const int32_t mantissa = float16_bits & MantissaMask;
     float16_bits >>= MantissaSize;
-    const int32_t exponent = float16_bits & ((1 << ExponentSize) - 1);
+    const int32_t exponent = float16_bits & ExponentMask;
     float16_bits >>= ExponentSize;
     const float sign = (float16_bits ? -1.f : 1.f);
 
